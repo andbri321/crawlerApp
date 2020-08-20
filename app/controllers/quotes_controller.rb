fc9b6eb0e5e78457f23de051f1b9db853cb241aa
andbri321 <andbri321@gmail.com>
2020-08-19 23:19:09 -0300

    [u] quotes_controller, add crawler

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,7 @@
+
+
 class QuotesController < ApplicationController
-  before_action :set_quote, only: [:show]
+  before_action :set_quotes, only: [:show]
 
   def show
     render json: @quotes
@@ -7,7 +9,12 @@ class QuotesController < ApplicationController
 
   private
 
-  def set_quote
+  def set_quotes
+    if Tag.where(name:params[:search_tag]).empty?
+      crawler = Crawler.new
+      crawler.get_quotes('http://quotes.toscrape.com/',params[:search_tag])
+    end
+
     quote_ids = Tag.where(name:params[:search_tag]).pluck(:quote_id)
     @quotes = Quote.where(:id.in => quote_ids )
   end
